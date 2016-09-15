05f4aa4c68b2e08cc065b2e0c4f7166494a3be7a
Crystalcxd <547339930@qq.com>
2016-09-16 01:39:35 +0800

    pod update

--- Pods/Target Support Files/Pods-BabySleep/Pods-BabySleep-resources.sh
+++ Pods/Target Support Files/Pods-BabySleep/Pods-BabySleep-resources.sh
@@ -87,6 +87,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "ELCImagePickerController/Classes/ELCImagePicker/Resources/Overlay.png"
   install_resource "ELCImagePickerController/Classes/ELCImagePicker/Resources/Overlay@2x.png"
   install_resource "MJRefresh/MJRefresh/MJRefresh.bundle"
+  install_resource "WeiboSDK/libWeiboSDK/WeiboSDK.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "AVOSCloud/AVOS/AVOSCloud/AVOSCloud_Art.inc"
@@ -96,6 +97,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "ELCImagePickerController/Classes/ELCImagePicker/Resources/Overlay.png"
   install_resource "ELCImagePickerController/Classes/ELCImagePicker/Resources/Overlay@2x.png"
   install_resource "MJRefresh/MJRefresh/MJRefresh.bundle"
+  install_resource "WeiboSDK/libWeiboSDK/WeiboSDK.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
